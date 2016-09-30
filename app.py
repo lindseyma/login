2846f20d2be32730051cd5c8359e614baf1bdf0c
lindseyma <lma2@stuy.edu>
2016-09-30 02:18:58 -0400

    whoooops

--- app.py
+++ app.py
@@ -7,12 +7,14 @@ app = Flask(__name__)
 def display():
     return render_template("form.html")
 
-@app.route('/authenticate/', methods = ["POST"])
+@app.route('/result/', methods = ["POST"])
 def auth():
     ##set something equal to request.form
     if request.form['user'] == "lma" and request.form['pass']=="cool":
-        return render_template("it worked")
-    return render_template("ouch")
+        ##returnMsg="it worked"
+        return render_template("result.html", returnMsg="woot you're in")
+    ##returnMsg="it didn't work"
+    return render_template("result.html", returnMsg="it didn't work")
 
 if __name__ ==  '__main__':
     app.debug = True
