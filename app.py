5315351bd9d99db32308dbcfd63df58f4d1548e1
lindseyma <lma2@stuy.edu>
2016-10-05 23:14:14 -0400

    late oops

--- app.py
+++ app.py
@@ -1,20 +1,38 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, hashlib, csv
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/login/')
 def display():
+    return render_template("home.html")
+
+@app.route('/login/')
+def dispLogin():
     return render_template("form.html")
 
 @app.route('/result/', methods = ["POST"])
 def auth():
-    ##set something equal to request.form
-    if request.form['user'] == "lma" and request.form['pass']=="cool":
-        ##returnMsg="it worked"
-        return render_template("result.html", returnMsg="woot you're in")
-    ##returnMsg="it didn't work"
-    return render_template("result.html", returnMsg="it didn't work")
+    ##login
+    if request.form["action"]=="login":
+        username=request.form["user"]
+        password=request.form["pass"]
+        h=hashlib.md5(password)
+        x=open('data/users.csv','r')
+        for acc in x:
+            if username == acc[0]:
+                if h.hexdigest()==acc[1]:
+                    return render_template("result.html", returnMsg="woot you're in")
+                return render_template("result.html", returnMsg="it didn't work")
+    else:
+        username=request.form["user"]
+        password=request.form["pass"]
+        x=open('data/users.csv', 'a')
+        for acc in data:
+            if username==acc[0]:
+                return render_template("result.html", returnMsg="username taken")
+        newUser=csv.writer('data/users.csv')
+        newUser.writerow([username,hashlib.md5(password).hexdigest()])
+        return render_template("result.html", returnMsg="you're in"
 
 if __name__ ==  '__main__':
     app.debug = True
