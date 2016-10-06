62830eef36af6674934e349a1a104cd60c52ecb4
lindseyma <lma2@stuy.edu>
2016-10-05 23:24:53 -0400

    tweaks

--- app.py
+++ app.py
@@ -18,21 +18,20 @@ def auth():
         password=request.form["pass"]
         h=hashlib.md5(password)
         x=open('data/users.csv','r')
-        for acc in x:
-            if username == acc[0]:
-                if h.hexdigest()==acc[1]:
+        for line in x:
+            if username == line.split(',')[0]:
+                if h.hexdigest()==line[1]:
                     return render_template("result.html", returnMsg="woot you're in")
                 return render_template("result.html", returnMsg="it didn't work")
     else:
         username=request.form["user"]
         password=request.form["pass"]
         x=open('data/users.csv', 'a')
-        for acc in data:
-            if username==acc[0]:
+        for line in x:
+            if username == line.split(',')[0]:
                 return render_template("result.html", returnMsg="username taken")
-        newUser=csv.writer('data/users.csv')
-        newUser.writerow([username,hashlib.md5(password).hexdigest()])
-        return render_template("result.html", returnMsg="you're in"
+        x.write(username+","+password+"\n")
+        return render_template("result.html", returnMsg="you're in")
 
 if __name__ ==  '__main__':
     app.debug = True
